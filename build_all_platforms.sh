c10c6492a236c6153a625aa84b6f3269f40dbeb1
Bruno Robert <bruno.jeanluke@icloud.com>
2025-04-16 23:24:33 +0200

    chore: update build script to rename binaries

--- build_all_platforms.sh
+++ build_all_platforms.sh
@@ -1,3 +1,9 @@
-cargo build --release
-cargo build --release --target x86_64-unknown-linux-gnu
-cargo xwin build --release --target x86_64-pc-windows-msvc
+# Get the parent folder path of this script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cargo build --release --target aarch64-apple-darwin \
+    && mv $SCRIPT_DIR/target/aarch64-apple-darwin/release/rsm $SCRIPT_DIR/target/aarch64-apple-darwin/release/rsm-macos-arm
+cargo build --release --target x86_64-unknown-linux-gnu \
+    && mv $SCRIPT_DIR/target/x86_64-unknown-linux-gnu/release/rsm $SCRIPT_DIR/target/x86_64-unknown-linux-gnu/release/rsm-linux-x86_64
+cargo xwin build --release --target x86_64-pc-windows-msvc \
+    && mv $SCRIPT_DIR/target/x86_64-pc-windows-msvc/release/rsm.exe $SCRIPT_DIR/target/x86_64-pc-windows-msvc/release/rsm-windows-x86_64.exe
